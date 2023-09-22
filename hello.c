3765d6d809a1b25cba2552708e2a73b9a159c28c
blackpencil123 <136896288+blackpencil123@users.noreply.github.com>
2023-09-22 10:28:08 +0000

    Fri, Sep 22, 2023, 10:28 AM +00:00

--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<cs50.h>
 int main(void){
-   int vu = get_int("enter number \n");
+   while(){
+    int vu = get_int("enter number \n");
+   }
       for (int i=0 ; i< vu ; i++){
 printf("#");
       }
